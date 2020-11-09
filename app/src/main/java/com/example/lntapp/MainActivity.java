7a9a33c87a9793b8fa6c2378e3e74c721e58c10e
99002765 <Erukulla.Sandeep@ltts.com>
2020-11-09 17:52:24 +0530

    Add login.

--- app/src/main/java/com/example/lntapp/MainActivity.java
+++ app/src/main/java/com/example/lntapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lntapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void cancelClick(View view) {
+        Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+        TextView welcomeTextView;
+        welcomeTextView=findViewById(R.id.textViewWelcome);
+        welcomeTextView.setText("Welcome to L&T");
+    }
+
+    public void loginClick(View view) {
+        EditText userName,pwd;
+        userName=findViewById(R.id.userName);
+        pwd=findViewById(R.id.password);
+        String uname=userName.getText().toString();
+        String pass=pwd.getText().toString();
+        if(uname.equals("Sandeep") && pass.equals("123456")){
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+        }
+        TextView welcomeTextView;
+        welcomeTextView=findViewById(R.id.textViewWelcome);
+        welcomeTextView.setText("Username: "+uname+" Password: "+pass);
     }
 }
