432118acc1356bbff4d177349f9b8bde3de4b5c7
99002765 <Erukulla.Sandeep@ltts.com>
2020-11-10 17:02:37 +0530

    Add listview with array adapter.

--- app/src/main/java/com/example/lntapp/HomeActivity.java
+++ app/src/main/java/com/example/lntapp/HomeActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    String[] languages;
     private static final String TAG = HomeActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        languages= new String[]{"Hindi","Telugu","Tamil","Malayalam"};
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         if(extras!=null) {
@@ -21,6 +25,11 @@ public class HomeActivity extends AppCompatActivity {
             TextView view = findViewById(R.id.showUserName);
             view.setText("Hello, " + data);
         }
+        ListView countriesListView = findViewById(R.id.countriesListview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, //layout file of each row in the listview
+                languages);
+        countriesListView.setAdapter(adapter);
     }
     @Override
     protected void onStart() {
